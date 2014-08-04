7e36051bfc670aa3f6bc93675e76c368091e5e6e
John Lozano <john.lozano@rackspace.com>
2014-08-04 18:28:29 -0500

    ui changes and grunt server update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@ var deps = [
 ];
 
 angular.module('upstatApp', deps)
-    .config(function ($routeProvider, $locationProvider, $httpProvider) {
+    .config(function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/home', {
                 templateUrl: 'views/home/home.html',
@@ -27,8 +27,7 @@ angular.module('upstatApp', deps)
                 templateUrl: 'views/status/status.html',
                 controller: 'StatusCtrl'
             })
-            .when('/teams/:id/statuses'
-              ,{
+            .when('/teams/:id/statuses', {
                 templateUrl: 'views/team/teamStatus.html',
                 controller: 'TeamStatusCtrl'
             })
